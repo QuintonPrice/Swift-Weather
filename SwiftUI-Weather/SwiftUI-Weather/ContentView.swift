32f4c053f5d56c267e4be82d21a849f1c9bcd12c
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2022-05-19 21:52:06 -0700

    Functional API call now works

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -7,8 +7,113 @@
 
 import SwiftUI
 
+// holds all data from the JSON request
+struct WeatherJSON: Codable, Hashable {
+    
+    // checking stoof
+    static func == (lhs: WeatherJSON, rhs: WeatherJSON) -> Bool {
+        lhs.name == rhs.name
+    }
+    
+    // hashing stoof
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+    
+    var coord: Coord            // coordinate struct
+    var weather: [Weather]      // array of Weather struct
+    var base: String            // "internal parameter..?"
+    var main: Main              // main struct (contains the juicy data)
+    var visibility: Int         // visibility number
+    var wind: Wind              // wind struct
+    var clouds: Clouds          // clouds struct
+    var dt: Int                 // time of data calculation, unix, UTC
+    var sys: Sys                // internal parameer
+    var timezone, id: Int       // timezone
+    var name: String            // city namme
+    var cod: Int                // another internal parameter (..?)
+}
+
+struct Clouds: Codable {
+    var all: Int
+    init() {
+        all = 0
+    }
+}
+
+struct Coord: Codable {
+    var lon, lat: Double
+    init() {
+        lon = 32
+        lat = 64
+    }
+}
+
+struct Main: Codable {
+    var temp: Double
+    var pressure: Int
+    var humidity: Int
+    var temp_min: Double
+    var temp_max: Double
+    
+    init() {
+        temp = 0.0
+        pressure = 0
+        humidity = 0
+        temp_min = 0.0
+        temp_max = 0.0
+    }
+}
+
+struct Sys: Codable {
+    var type, id: Int
+    var country: String
+    var sunrise, sunset: Int
+    
+    init() {
+        type = 0
+        id = 0
+        country = "USA"
+        sunrise = 1000
+        sunset = 2300
+    }
+}
+
+struct Weather: Codable, Hashable {
+    
+    // checking stoof
+    static func == (lhs: Weather, rhs: Weather) -> Bool {
+        lhs.main == rhs.main
+    }
+    
+    // hashing stoof
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(main)
+    }
+    
+    var id: Int
+    var main, description, icon: String
+    
+    init() {
+        id = 1
+        main = "Test main"
+        description = "Yep, it's raining"
+        icon = "9d0"
+    }
+}
+
+struct Wind: Codable {
+    var speed: Double
+    var deg: Int
+    
+    init() {
+        speed = 100
+        deg = 100
+    }
+}
+
 // struct used for weather array
-struct Weather {
+struct DayOfWeekWeather {
     let id = UUID()
     
     let dayOfWeek: String
@@ -16,19 +121,31 @@ struct Weather {
     let temperature: Int
 }
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackID: Int
+    var trackName: String
+    var collectionName: String
+}
+
 // Main ContentView
 struct ContentView: View {
     
     // used for darkmode
     @State private var isNight = false
+    @State private var results = [Result]()
+    @State private var weatherArray: [WeatherJSON] = []
     
     // populate weather array
     let weather = [
-        Weather(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 57),
-        Weather(dayOfWeek: "WED", imageName: "cloud.moon.rain.fill", temperature: 52),
-        Weather(dayOfWeek: "THU", imageName: "sun.dust.fill", temperature: 72),
-        Weather(dayOfWeek: "FRI", imageName: "cloud.sun.fill", temperature: 49),
-        Weather(dayOfWeek: "SAT", imageName: "cloud.sun.fill", temperature: 53),
+        DayOfWeekWeather(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 57),
+        DayOfWeekWeather(dayOfWeek: "WED", imageName: "cloud.moon.rain.fill", temperature: 52),
+        DayOfWeekWeather(dayOfWeek: "THU", imageName: "sun.dust.fill", temperature: 72),
+        DayOfWeekWeather(dayOfWeek: "FRI", imageName: "cloud.sun.fill", temperature: 49),
+        DayOfWeekWeather(dayOfWeek: "SAT", imageName: "cloud.sun.fill", temperature: 53),
     ]
     
     
@@ -38,29 +155,65 @@ struct ContentView: View {
             
             VStack {
                 CityTextView(cityName: "Seattle, WA")
-                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 46)
-                
-                // 5 day forecast
-                HStack(spacing: 20) {
-                    ForEach(weather, id: \.id) { weather in
-                        WeatherDayView(dayOfWeek: weather.dayOfWeek,
-                                       imageName: weather.imageName,
-                                       temperature: weather.temperature)
-                    }
+                ForEach(weatherArray, id: \.self) { result in
+                    MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: Int(convertTemperature(temp: (result.main.temp), from: .kelvin, to: .fahrenheit)))
                 }
+                
+//                // 5 day forecast
+//                HStack(spacing: 20) {
+//                    ForEach(weather, id: \.id) { weather in
+//                        WeatherDayView(dayOfWeek: weather.dayOfWeek,
+//                                       imageName: weather.imageName,
+//                                       temperature: weather.temperature)
+//                    }
+//                }
                 
                 Spacer()
                 
+                // toggle dark mode button
                 Button {
                     isNight.toggle() // toggles boolean value
                 } label: {
                     WeatherButton(title: "Toggle Dark Mode", textColor: .blue, backgroundColor: .white)
                 }
-                
-                Spacer()
             }
         }
+        .task { await handleData() } // loads all data
     }
+    
+    func handleData() async {
+        
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=673c2c51f8a9ae9ceacaee7a8e3aa885") else {
+            print("This URL does not work!")
+            return
+        }
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            let (weatherData, _) = try await URLSession.shared.data(from: url)
+            if let weatherObj = try? decoder.decode(WeatherJSON.self, from: weatherData) {
+                weatherArray.append(weatherObj)
+                // print(weatherArray[0])
+            }
+            
+        } catch {
+            print("Did not work :(")
+        }
+    }
+    
+    // Converts the temperature
+    func convertTemperature(temp: Double, from inputTempType: UnitTemperature, to outputTempType: UnitTemperature) -> Double {
+        let input = Measurement(value: temp, unit: inputTempType)
+        let output = input.converted(to: outputTempType)
+        return output.value
+    }
+//
+//    // Converts temperature to appropriate units
+//    func convertTemp(temp: Double, units: String) -> Int {
+//
+//        return 0
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -79,6 +232,7 @@ struct WeatherDayView: View {
     
     var body: some View {
         VStack {
+            
             Text(dayOfWeek)
                 .font(.system(size: 16, weight: .medium, design: .default))
                 .foregroundColor(.white)
@@ -143,29 +297,5 @@ struct MainWeatherStatusView: View {
 //
 //    var body: some View {
 //        HStack(spacing: 20) {
-//            WeatherDayView(dayOfWeek: "TUE",
-//                           imageName: "cloud.sun.fill",
-//                           temperature: 57)
-//
-//            WeatherDayView(dayOfWeek: "WED",
-//                           imageName: "cloud.moon.rain.fill",
-//                           temperature: 52)
-//
-//            WeatherDayView(dayOfWeek: "THU",
-//                           imageName: "sun.dust.fill",
-//                           temperature: 72)
-//
-//            WeatherDayView(dayOfWeek: "FRI",
-//                           imageName: "cloud.sun.fill",
-//                           temperature: 49)
-//
-//            WeatherDayView(dayOfWeek: "SAT",
-//                           imageName: "cloud.sun.fill",
-//                           temperature: 53)
-//
-//            WeatherDayView(dayOfWeek: "SUN",
-//                           imageName: "cloud.sun.fill",
-//                           temperature: 55)
-//        }
 //    }
 //}
